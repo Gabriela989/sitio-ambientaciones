970ddff6fe159886cb401234bbb8c22dc97b60d7
gabriela nuñez <gabrielanoeminunez@gmail.com>
2023-08-11 00:42:14 -0300

    m4uni4 y m5uni1

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
-const { captureRejectionSymbol } = require('nodemailer/lib/xoauth2');
-router.get('/', function(req, res, next) {
+var novedadesModel = require('../../models/novedadesModel');
+
+
+/*GET novedades page.*/
+
+router.get('/', async function (req, res, next) {
+    var novedades = await novedadesModel.getNovedades();
     res.render('admin/novedades',{
         layout: 'admin/layout',
-        usuario:req.session.nombre
+        usuario:req.session.nombre,
+        novedades
     });
 });
 
+
+/* para eliminar una novedad */
+router.get('/eliminar/:id',async (req,res,nesxt)=>{
+    const id =req.params.id;
+    await novedadesModel.deletenovedadesById(id);
+    res.redirect('/admin/novedades')
+
+}); // cierra get de eliminar 
 module.exports = router;
